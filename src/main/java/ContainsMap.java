ae5549fca615ee39258ae40e6505da92065d6654
PersesTitan <torampersesTitan@gmail.com>
2022-12-04 10:30:08 +0900

    create ContainsMap

---
+++ src/main/java/ContainsMap.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class ContainsMap<V>  extends HashMap<String, V> {
+
+    @Override
+    public boolean containsKey(Object key) {
+        return getVal(key.toString()) != null;
+    }
+
+    @Override
+    public V get(Object key) {
+        return getVal(key.toString());
+    }
+
+    @Override
+    public V remove(Object key) {
+        String k = key.toString();
+        for (Entry<String, V> entry : super.entrySet()) {
+            if (entry.getKey().contains(k)) return entry.getValue();
+        }
+        return null;
+    }
+
+    /**
+     * No Override<br>
+     * can use : private
+     */
+    private V getVal(String key) {
+        for (Entry<String, V> entry : super.entrySet()) {
+            if (entry.getKey().contains(key)) return entry.getValue();
+        }
+        return null;
+    }
+
+    /**
+     * No Override<br>
+     * can use : public
+     */
+    public List<V> gets(Object key) {
+        String k = key.toString();
+        List<V> list = new ArrayList<>();
+        for (Entry<String, V> entry : super.entrySet()) {
+            if (entry.getKey().contains(k)) list.add(entry.getValue());
+        }
+        return list;
+    }
+
+    public List<V> removes(Object key) {
+        String k = key.toString();
+        List<V> list = new ArrayList<>();
+        for (Entry<String, V> entry : super.entrySet()) {
+            if (entry.getKey().contains(k)) {
+                V v = entry.getValue();
+                list.add(super.remove(v));
+            }
+        }
+        return list;
+    }
+}
